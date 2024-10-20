29c69278fae6dcf14f058fe23596526be21364b9
Ian Buzzo <german.buzzo12@gmail.com>
2024-10-19 20:33:53 -0600

    changed .gitignore

--- express-workshop/index.js
+++ express-workshop/index.js
@@ -1,19 +1,28 @@
+//Dependencias
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
+//Routers
+const pokemon = require ('./routes/pokemon');
+const user = require('./routes/user');
+//Middleware
+const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const index = require ('./middleware/index');
 
-/*
-Verbos HTTP
-GET ---- PETICION AL SERVIDOR
-POST---GUARDAR O PUBLICAR ALGO 
-PATCH----ACTUALIZACION DE UN DATO ESPECIFICO
-PUT-----ACTUALIZAR TODOS LOS ELEMENTOS
-DELETE
-*/ 
-app.get('/', (req, res, next) =>{ 
-    res.status(200);
-    res.send('Bienvenido');
-} )
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
+
+
+app.use("/", index);
+app.use("/user", user);
+app.use(auth);
+app.use("/pokemon", pokemon);
+app.use(notFound);
+
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running...');
 });
